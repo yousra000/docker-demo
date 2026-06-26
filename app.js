5c8e38a60fdf31c8b9e44fc81eb865401b884eda
yousra000 <yousraramadangad1@gmail.com>
2026-06-26 23:07:49 +0300

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+
+    res.end("Hello from Docker!");
+});
+
+server.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});
